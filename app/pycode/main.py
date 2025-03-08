d491195e576a52856af64de4d0a5b1c5072daefe
Matt Dilley <dizimatt74@gmail.com>
2025-03-08 16:54:47 +0800

    working ollama embeds with single-product pdf

--- app/pycode/main.py
+++ app/pycode/main.py
@@ -1,13 +1,15 @@
+# import ollama
+import ollama
+from langchain_community.document_loaders import PyMuPDFLoader,PDFPlumberLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+
 from fastapi import FastAPI, WebSocket
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_community.document_loaders import PDFPlumberLoader  
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.vectorstores import FAISS  
-from langchain_ollama import OllamaLLM
+import uvicorn
 import json
+import logging
+
 # from langchain_core.prompts.prompt import PromptTemplate
 
 class ConnectionManager:
@@ -28,64 +30,89 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         """disconnect event"""
         self.active_connections.remove(websocket)
+#def process_pdf(pdf_bytes):
+def process_pdf(filepath:str): 
+    logger.info(f"starting to process the pdf:{filepath}")
+    # ./docs/products_single.pdf
 
-def call_ollama(message):
-    print("gotting the docs from the pdf file")
-    loader = PDFPlumberLoader("./docs/products.pdf")  
-#    loader = PDFPlumberLoader("./docs/products_single.pdf")  
-    docs = loader.load() 
-    print("got the docs from the pdf file")
+    '''
+    if pdf_bytes is None:
+        return None, None, None
+    
+    loader = PyMuPDFLoader(pdf_bytes)
+    '''
 
-    # Split text into semantic chunks  
-    text_splitter = SemanticChunker(HuggingFaceEmbeddings())  
-    documents = text_splitter.split_documents(docs)
-    print("splitted the docs into semantic chunks")
+    loader = PDFPlumberLoader(filepath)  
 
-    # Generate embeddings  
-    embeddings = HuggingFaceEmbeddings()  
-    vector_store = FAISS.from_documents(documents, embeddings)  
-    print("generated embeddings")
+    data = loader.load()
 
-    # Connect retriever  
-    retriever = vector_store.as_retriever(search_kwargs={"k": 3})  # Fetch top 3 chunks
-    print ("created retriever")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=100
+    )
+    chunks = text_splitter.split_documents(data)
+    embeddings = OllamaEmbeddings(model="mxbai-embed-large:latest", base_url='http://ollama:11434')
+    
 
-    llm = OllamaLLM(
+    vectorstore = Chroma.from_documents(
+        documents=chunks, embedding=embeddings, persist_directory="./chroma_db"
+    )
+    retriever = vectorstore.as_retriever()
+    logger.info("procesed the pdf! you can now proceed...")
+
+    return text_splitter, vectorstore, retriever
+
+def combine_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def ollama_llm(question, context):
+    logger.info("about to deepseek with embeddings")
+    formatted_prompt = f"Question: {question}\n\nContext: {context}"
+
+    client = ollama.Client(
+        host='http://ollama:11434'
+    )
+    response = client.chat(
         model="deepseek-r1:1.5b",
-        base_url="http://ollama:11434",
-    )
-    print("created llm")
-
-    system_prompt = (
-        "Use the given context to answer the question. "
-        "If you don't know the answer, say you don't know. "
-        "Use three sentence maximum and keep the answer concise. "
-        "Context: {context}"
+        messages=[
+            {"role": "user", "content": formatted_prompt},
+        ],
     )
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            ("human", "{input}"),
-        ]
-    )
-    print("created chatprompt")
+    response_content = response["message"]["content"]
+    logger.info(f"response {response_content}")
 
-    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    print("created documents chain")
+    # Remove content between <think> and </think> tags to remove thinking output
+    # final_answer = re.sub(r"<think>.*?</think>", "", response_content, flags=re.DOTALL).strip()
 
-    chain = create_retrieval_chain(retriever, question_answer_chain)
-    print("attached retriever to the documents chain")
+    return response_content
 
-    results = chain.invoke({"input": message})
-    print(f"query response: {results}")
-    return results
+def rag_chain(question, text_splitter, vectorstore, retriever):
+    retrieved_docs = retriever.invoke(question)
+    formatted_content = combine_docs(retrieved_docs)
+    return ollama_llm(question, formatted_content)
+
+async def call_ollama(question: str, websocket: WebSocket):
+#    text_splitter, vectorstore, retriever = process_pdf("./docs/products_single.pdf")
+
+    if text_splitter is None:
+        return None  # No PDF uploaded
+
+    result = rag_chain(question, text_splitter, vectorstore, retriever)
+    await manager.send_personal_message(result,websocket)
+
+    return result
 
 app = FastAPI()
 manager = ConnectionManager()
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/usr/src/openint-py.log', encoding='utf-8', level=logging.INFO)
+#logging.basicConfig(filename='./openint-py.log', encoding='utf-8', level=logging.INFO)
+# one-time on-load process to retrieve the files for indexing, and start the ollama server
+text_splitter, vectorstore, retriever = process_pdf("./docs/products_single.pdf")
 
 @app.get("/")
 def read_root():
+    logger.info("running from read_root")
     return {"Hello": "World"}
 
 
@@ -96,14 +123,13 @@ def read_item(item_id: int, q: str | None = None):
 
 @app.websocket("/api/ollama/send-message-ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
     try:
+        await manager.connect(websocket)
         data = await websocket.receive_text()
         data_obj = json.loads(data)
-        await manager.send_personal_message(f"Received (nodaemon):{data_obj["question"]}",websocket)
-        results = call_ollama(data_obj["question"])
-        print (f"results:{results['answer']}")
-        await manager.send_personal_message(f"results:{results['answer']}",websocket)
+
+        logger.info(f"about to call ollama with question: {data_obj['question']}")
+        results_string = await call_ollama(data_obj["question"], websocket)
         await manager.send_personal_message(f"<aitextdone />",websocket)
             
     except WebSocketDisconnect:
@@ -114,4 +140,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == '__main__':
     print("running from __main__")
-
+#    uvicorn.run(app, host="0.0.0.0", port=8080)
+#    process_pdf("./docs/products_single.pdf")
